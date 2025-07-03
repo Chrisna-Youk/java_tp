564a37393b480fd59664395651d4d71cbd731a4d
Chrisna Youk <cyouk@myges.com>
2025-07-03 16:30:57 +0200

    [TEST] 2 neighbours alive

--- src/test/java/fr/esgi/AppTest.java
+++ src/test/java/fr/esgi/AppTest.java
@@ -36,4 +36,14 @@ public class AppTest {
 
         assertThat(grid.getCell(2, 1).isAlive()).isEqualTo(false);
     }
+
+    @Test
+    public void alive_2_neighbours_expect_alive() {
+        grid.getCell(2, 1).live();
+        grid.getCell(3, 1).live();
+        grid.getCell(1, 2).live();
+
+        grid.run();
+        assertThat(grid.getCell(2, 1).isAlive()).isEqualTo(true);
+    }
 }
