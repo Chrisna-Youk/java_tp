417195234882a348c1dae3c1508e468b889b45bf
Jean <jeanalbalouh@gmail.com>
2025-07-03 14:49:14 +0200

    add Cell class

---
+++ src/main/java/fr/esgi/Cell.java
@@ -0,0 +1,13 @@
+package fr.esgi;
+
+public class Cell {
+    int x;
+    int y;
+    boolean alive;
+
+    Cell(int x, int y, boolean alive) {
+        this.x = x;
+        this.y = y;
+        this.alive = alive;
+    }
+}
